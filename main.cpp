9d4f94ddbdc86a02b2f6718910942a2ebcd7bf27
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2023-10-11 15:33:41 +0900

    2-3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include<cstdio>
 #include<cstdlib>
 
-int main()
+int main(int argc, char* argv[])
 {
-	printf("Hello,World!!\n");
-
+	for (int i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		printf("\n");
+	}
 	system("pause");
+
 	return 0;
 }
